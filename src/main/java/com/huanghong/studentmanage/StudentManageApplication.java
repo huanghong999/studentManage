43b6aa696d8cecf2ef9f7036321599ef8425041f
huanghong <64505294@qq.com>
2024-02-12 13:39:07 +0800

    第一个应用程序

--- src/main/java/com/huanghong/studentmanage/StudentManageApplication.java
+++ src/main/java/com/huanghong/studentmanage/StudentManageApplication.java
@@ -2,6 +2,10 @@ package com.huanghong.studentmanage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class StudentManageApplication {
@@ -10,4 +14,19 @@ public class StudentManageApplication {
         SpringApplication.run(StudentManageApplication.class, args);
     }
 
+    /**
+     * hello world.
+     *
+     * @return hello
+     */
+    @GetMapping("/a")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("hello world", HttpStatus.OK);
+    }
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello1(){
+        return "hello 11";
+    }
 }
