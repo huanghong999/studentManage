1fabdd2946430d43e1cc8aa0e28ff2edcc75c5c7
huanghong <64505294@qq.com>
2024-02-12 13:50:22 +0800

    第一个应用程序-修复bug:缺少访问注解 @RestController

--- src/main/java/com/huanghong/studentmanage/StudentManageApplication.java
+++ src/main/java/com/huanghong/studentmanage/StudentManageApplication.java
@@ -2,31 +2,21 @@ package com.huanghong.studentmanage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentManageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManageApplication.class, args);
     }
 
-    /**
-     * hello world.
-     *
-     * @return hello
-     */
-    @GetMapping("/a")
-    public ResponseEntity<String> hello() {
-        return new ResponseEntity<>("hello world", HttpStatus.OK);
-    }
-
     @GetMapping("hello")
     @ResponseBody
-    public String hello1(){
+    public String hello1() {
         return "hello 11";
     }
 }
